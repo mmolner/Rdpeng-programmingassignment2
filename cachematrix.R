5795f7dfd161555be937385c838fa5dbf3174a48
SOHAM KULKARNI <50440348+skk2106@users.noreply.github.com>
2019-05-13 13:22:05 +0530

    cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,28 @@
+makeCacheMatrix <- function(x = matrix()) {
+
+inverse_x <- NULL
+set <- function(y) {
+x <<- y
+inverse_x <<- NULL
+}
+get <- function() x
+setinverse <- function(inverse) inverse_x <<- inverse
+getinverse <- function() inverse_x
+list(set = set,
+get = get,
+setinverse = setinverse ,
+getinverse = getinverse)
+
+}
+
+cacheSolve <- function(x, ...) {
+  inverse_x <- x$getinverse()
+  if(!is.null(inverse_x)) {
+    message("Getting cached data.")
+    return(inverse_x)
+  }
+  data <- x$get()
+  inverse_x <- solve(data)
+  x$setinverse(inverse_x)
+  inverse_x
+}
